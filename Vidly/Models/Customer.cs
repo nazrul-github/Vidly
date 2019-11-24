aebdd9fa6e70041fec2435e424353521a2685621
Robin <nozrulgithub@gmail.com>
2019-11-24 19:21:58 +0600

    updated master

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Vidly.Models
 {
-    public class Customer 
+    public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-       
     }
 }
